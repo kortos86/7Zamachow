6f32c71a7054a4a3f50d092b8f34e26035b98ccd
kortos86 <kortos86@gmail.com>
2016-06-20 16:06:34 +0200

    First version of app project. To be largely changed

---
+++ app/src/main/java/com/artpi/games/a7zamachow/A3CardsGame.java
@@ -0,0 +1,13 @@
+package com.artpi.games.a7zamachow;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class A3CardsGame extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_a3_cards_game);
+    }
+}
