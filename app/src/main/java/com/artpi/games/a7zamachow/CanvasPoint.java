2a78b6c35002140d1afe80dc1dd94c8edd931555
kortos86 <kortos86@gmail.com>
2016-11-29 16:24:56 +0100

    Pixel measurments improvements to 3 cards

---
+++ app/src/main/java/com/artpi/games/a7zamachow/CanvasPoint.java
@@ -0,0 +1,31 @@
+package com.artpi.games.a7zamachow;
+
+/**
+ * Created by kortm on 7/13/2016.
+ */
+
+public class CanvasPoint {
+    private int x;
+    private int y;
+
+    public CanvasPoint(int x, int y)
+    {
+        this.x = x;
+        this.y = y;
+    }
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+}
